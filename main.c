5c99941eb15f898bc8d491d77a8d95258a1bda55
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2018-09-10 22:32:22 +0200

    Test message sending
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
-#ifndef F_CPU
-#define F_CPU 1000000
-#endif
-
+#include "defines.h"
 #include <util/delay.h>
+#include <stdlib.h>
 #include "led.h"
+#include "message.h"
+/* #include "uart.h" */
 
 int main(){
 	led_init();
+	message_init();
 
 	while(1){
+		message_write("Hello world\n\r");
+
 		led_turn_on();
 		_delay_ms(1000);
 
