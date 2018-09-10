ff100ccd271fa65475155494a5c5600ab1ab4449
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2018-09-10 22:31:08 +0200

    Wrote UART driver
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

---
+++ uart.c
@@ -0,0 +1,25 @@
+#include <avr/io.h>
+#include <stdint.h>
+#include "defines.h"
+#include "uart.h"
+#define BAUD_9600 (F_CPU/16/9600 - 1)
+
+void uart_init(){
+	UBRR0H = (uint8_t)(BAUD_9600 >> 8);
+	UBRR0L = (uint8_t)(BAUD_9600);
+
+	UCSR0A &= ~(1 << U2X0);
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+	UCSR0C = (1 << URSEL0) | (1 << UCSZ00) | (1 << UCSZ01);
+}
+
+void uart_write(char letter){
+	while(!(UCSR0A & (1 << UDRE0)));
+	UDR0 = letter;
+}
+
+char uart_read(){
+	if(!(UCSR0A & (1 << RXC0)))
+		return '\0';
+	return UDR0;
+}
---
+++ uart.h
@@ -0,0 +1,10 @@
+#ifndef UART_H
+#define UART_H
+
+void uart_init();
+
+void uart_write(char letter);
+
+char uart_read();
+
+#endif
