3f58ac7decd8dd6e2b674955efef4588490db91a
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2018-10-11 13:02:19 +0200

    Added UART support to ubit library
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- ble_code/main.c
+++ ble_code/main.c
@@ -1,6 +1,22 @@
+#include <stdint.h>
 #include "ubit.h"
 
+/* #include "ble.h" */
+
 int main(){
+	/* uint32_t err_code = 0; */
+
+	/* sd_softdevice_enable(NULL, NULL); */
+
+	ubit_uart_init();
+
+	while(1){
+		ubit_uart_print(
+			"Here are some nubers: |%6d|, |%d|, |%6d|, |%d|\n\r",
+			12, -751, -7, 571
+		);
+	}
+
 	ubit_led_matrix_init();
 	ubit_buttons_init();
 
--- ble_code/ubit.h
+++ ble_code/ubit.h
@@ -7,10 +7,16 @@ void ubit_led_matrix_turn_on();
 
 void ubit_led_matrix_turn_off();
 
+
 void ubit_buttons_init();
 
 int ubit_button_press_a();
 
 int ubit_button_press_b();
+
+
+void ubit_uart_init();
+
+void ubit_uart_print(char * p_format, ...);
 
 #endif
